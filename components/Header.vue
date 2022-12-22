7ece300cadddbda8a98ebb0f5c9cca86960c1646
Vinicin1101 <vserafim_o@outlook.com>
2022-12-22 20:11:30 -0300

    preparando alguns paths

---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <a href="profile" class="profile-link">
+      <img
+        src="http://www.gravatar.com/avatar/f8e067eb1582177fb3b223a6fddf279f?s=90&r=g&d=identicon"
+        alt="profile_picture"
+        class="profile_pic rounded-circle"
+        id="profile_pic"
+      />
+    </a>
+    <nav class="btns d-flex justify-content-center mb-3">
+      <ul>
+        <li class="btn-link mr-3">
+          <a href="#" class="btn btn-primary">Home</a>
+        </li>
+
+        <li class="btn-link mr-3">
+          <a href="projects" class="btn btn-primary">Projecs</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="clearfix"></div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 10px;
+}
+#profile_pic {
+  width: 50px;
+  height: 50px;
+}
+
+.profile-link {
+  float: left;
+  line-height: 65px;
+}
+</style>
---
+++ pages/spotify/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <Header></Header>
+    <p>
+      Estou trabalhando nisso, peço que volte mais tarde.
+      <i class="bx bxs-heart"></i>
+    </p>
+  </main>
+</template>
