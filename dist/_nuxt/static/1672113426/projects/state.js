a7f070ea6601640e25e75572b312ce0a848e7eb5
Vinicin1101 <vserafim_o@outlook.com>
2022-12-27 00:57:54 -0300

    dist

---
+++ dist/_nuxt/static/1672113426/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1672113426",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
